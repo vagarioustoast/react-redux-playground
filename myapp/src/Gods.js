c5a6957d0a9d9fbd29662b623142f022dd3487e7
vagarioustoast <vagarioustoast@gmail.com>
2019-04-17 14:06:43 -0700

    stateless components

--- myapp/src/Gods.js
+++ myapp/src/Gods.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Gods extends Component {
-  render() {
-    const { gods } = this.props;
-    const myGods = gods.map(god => {
-      return (
-        <div className="gods" key={god.id}>
-          <div>Name: {god.name}</div>
-          <div>Age: {god.age}</div>
-          <div>Belt: {god.b}</div>
-        </div>
-      );
-    });
-    return <div className="god-list">{myGods}</div>;
-  }
-}
+const Gods = ({ gods }) => {
+  const myGods = gods.map(god => {
+    return (
+      <div className="gods" key={god.id}>
+        <div>Name: {god.name}</div>
+        <div>Age: {god.age}</div>
+        <div>Belt: {god.b}</div>
+      </div>
+    );
+  });
+  return <div className="god-list">{myGods}</div>;
+};
+
+export default Gods;
