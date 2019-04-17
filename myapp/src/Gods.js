2dbb9cd2a9806f4d07c8c51c65310f3efb037ff8
vagarioustoast <vagarioustoast@gmail.com>
2019-04-17 14:23:27 -0700

    conditional output

--- myapp/src/Gods.js
+++ myapp/src/Gods.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const Gods = ({ gods }) => {
   const myGods = gods.map(god => {
-    return (
-      <div className="gods" key={god.id}>
-        <div>Name: {god.name}</div>
-        <div>Age: {god.age}</div>
-        <div>Belt: {god.b}</div>
-      </div>
-    );
+    if (god.age > 998)
+      return (
+        <div className="gods" key={god.id}>
+          <div>Name: {god.name}</div>
+          <div>Age: {god.age}</div>
+          <div>Belt: {god.b}</div>
+        </div>
+      );
+    else {
+      return null;
+    }
   });
   return <div className="god-list">{myGods}</div>;
 };
