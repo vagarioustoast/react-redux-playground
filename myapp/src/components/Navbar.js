d3170de52298bb1c58b6e929676e2d0a5278c497
vagarioustoast <vagarioustoast@gmail.com>
2019-04-22 14:41:13 -0700

    higher order components

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -24,4 +24,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
